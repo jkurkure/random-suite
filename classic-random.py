4cc4fce756a04ead0c5e4ecc1c9632f4d920533a
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-09-04 14:14:47 +0000

    plugging some holes

--- classic-random.py
+++ classic-random.py
@@ -6,6 +6,8 @@ prompt = "⚙️  > "
 moveUp = lambda:print('\033[1A', end="")
 clear = lambda n:print(("\b" * n) + (" " * n) + ("\b" * n), end="")
 
+quit = lambda:exit()
+
 dice = lambda:["⚄", "⚁", "⚃", "⚀", "⚅", "⚂"][int(random.random()*6)]
 card = lambda:f"{random.choice(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"])}{random.choice(["♠", "♣", "♥", "♦"])}"
 ipv4 = lambda:"{}.{}.{}.{}".format(*[int(random.randint(1234567, 891011121314))*x % 256 for x in range(1, 5)])
@@ -22,6 +24,9 @@ while True:
     clear(len(prompt) + len(cmd))
 
     try:
-        print(eval(f"{cmd}()"))
+        if type(eval(cmd)) is type(moveUp):
+            print(eval(f"{cmd}()"))
+        else:
+            raise NameError 
     except NameError:
         print("Command not found!")
